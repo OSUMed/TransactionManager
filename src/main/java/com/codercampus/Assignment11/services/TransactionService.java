2f1599c33a1e70bd57dd80c3d913ec8c6947f92d
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-09-02 13:53:15 -0700

    add working service findAll method ; add transaction page

--- src/main/java/com/codercampus/Assignment11/services/TransactionService.java
+++ src/main/java/com/codercampus/Assignment11/services/TransactionService.java
@@ -1,8 +1,21 @@
 package com.codercampus.Assignment11.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.codercampus.Assignment11.domain.Transaction;
+import com.codercampus.Assignment11.repository.TransactionRepository;
 
 @Service
 public class TransactionService {
-
+	@Autowired
+	TransactionRepository transactionRepo;
+	
+	public List<Transaction> findAll(){
+		List<Transaction> allTransactions = transactionRepo.findAll();
+		System.out.println("In service: " +allTransactions);
+		return allTransactions;
+	}
 }
--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -1,11 +1,14 @@
 package com.codercampus.Assignment11.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.codercampus.Assignment11.repository.TransactionRepository;
+import com.codercampus.Assignment11.domain.Transaction;
 import com.codercampus.Assignment11.services.TransactionService;
 
 @Controller
@@ -14,20 +17,19 @@ public class TransactionController {
 	@Autowired
 	TransactionService transactionService;
 	
-	@Autowired
-	TransactionRepository transactionRepo;
-	
 
 	
 	@GetMapping("/transactions")
-	public String getAllTransactions() {
-		
+	public String getAllTransactions(ModelMap model) {
+		List<Transaction> allTransactions = transactionService.findAll();
+		System.out.println("In controller: " + allTransactions);
+		model.put("transactions", allTransactions);
 		return "transactions";
 	}
 	
 	@GetMapping("/transactions/{transaction_id}")
 	public String getTransactionId(@PathVariable Long transaction_id) {
 		System.out.println("Transaction id is: " + transaction_id);
-		return "transactions";
+		return "transaction";
 	}
 }
