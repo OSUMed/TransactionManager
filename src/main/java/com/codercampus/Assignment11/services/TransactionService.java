dac526f0b4691c711011a32482e522f5d7f6ddc8
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-09-02 15:11:49 -0700

    add back button and add basic rendering of transaction_id page

--- src/main/java/com/codercampus/Assignment11/services/TransactionService.java
+++ src/main/java/com/codercampus/Assignment11/services/TransactionService.java
@@ -1,6 +1,7 @@
 package com.codercampus.Assignment11.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,16 @@ public class TransactionService {
 	public List<Transaction> findAll(){
 		List<Transaction> allTransactions = transactionRepo.findAll();
 		return allTransactions;
+	}
+
+	public Transaction getById(Long transaction_id) {
+		List<Transaction> allTransactions = transactionRepo.findAll();
+		List<Transaction> transaction_id_matches = allTransactions
+			.stream()
+			.filter(transaction -> transaction.getId().equals(transaction_id))
+			.collect(Collectors.toList());
+			
+			
+		return transaction_id_matches.get(0);
 	}
 }
